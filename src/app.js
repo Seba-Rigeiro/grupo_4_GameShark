cb0a36af031cc2f7326fb87d5f0db10303ac556c
rodrigo <rodrigoscolnic@gmail.com>
2021-02-04 20:37:29 -0300

    agregando configuraciones app.use

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 app.use(express.static('../public'));
 app.set('view engine', 'ejs');
 
