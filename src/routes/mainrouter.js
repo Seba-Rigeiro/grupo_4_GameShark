c4aae95f232f853deb602a6dee52a3865b8be4cf
Sebastian Rigeiro <seba_rigeiro@yahoo.com.ar>
2021-03-22 19:11:38 -0300

    ruta post, put, configuracion de multer, path y diskStorage

--- src/routes/mainrouter.js
+++ src/routes/mainrouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer = require ('multer');
 const mainController = require('../controllers/maincontroller');
 
 router.get('/' , mainController.index);
--- src/routes/productsrouter.js
+++ src/routes/productsrouter.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const multer = require ('multer');
+const path = require ('path')
 const productsController = require('../controllers/productscontroller');
 
-router.get('/create' , productsController.create);
+const storage = multer.diskStorage ({
+    destination: (req, file, callback) => {
+        callback (null, path.join(__dirname, '../../public/img/products'))
+    },
+    filename: (req, file, callback) => {
+        const newFlieName = 'product-' + Date.now() + path.extname (file.originalname)
+        callback (null, newFlieName)
+    }
+})
+
+const upload = multer ({ storage: storage})
+
+router.get('/create' , productsController.createForm);
+router.post('/create' , upload.single ('product_image'), productsController.create);
+
 router.get('/products/:id' , productsController.detail);
-router.get('/edit' , productsController.edit);
+
+router.get('/edit' , productsController.editForm);
+router.put('/edit' , productsController.edit);
 
 module.exports = router;
