69a4317a6321df83e33143f6c32b8fb67c7b4501
Sebastian Rigeiro <seba_rigeiro@yahoo.com.ar>
2021-03-22 21:10:50 -0300

    app.js actualizada

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const methodOverride = require ('method-override');
 
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(express.static('../public'));
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs');
 
 const mainRouter = require('./routes/mainrouter');
