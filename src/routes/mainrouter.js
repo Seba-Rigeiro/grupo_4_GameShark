8583f17ed994f755d6024e3fc35ef5d9a3fa6f74
Sebastian Rigeiro <seba_rigeiro@yahoo.com.ar>
2021-04-19 00:44:58 -0300

    correccion de ruta edit y agregado de ruta para listar por platform

--- src/routes/mainrouter.js
+++ src/routes/mainrouter.js
@@ -4,26 +4,28 @@ const multer = require ('multer');
 const {body} = require ('express-validator')
 const mainController = require('../controllers/maincontroller');
 
+// Validaciones para formulario de registro
 const validationsRegisterForm = [
     body ('name').notEmpty().withMessage ('Ingresa Nombre y Apellido'),
     body ('email').isEmail().withMessage ('Ingresa un email valido'),
     body ('password').isLength( { min:6, max:12 }).withMessage ('Ingresa una contraseña entre 6 y 12 caracteres'),
 ]
 
+// Validaciones para formulario de login
 const validationsLoginForm = [
     body ('email').isEmail().withMessage ('Ingresa un email valido'),
-    body ('password').isLength( { min:6, max:12 }).withMessage ('Ingresa una contraseña entre 6 y 12 caracteres'),
+    body ('password').notEmpty() .withMessage ('Ingresa una contraseña'),
 ]
 
-
+// Ruta pagina principal
 router.get('/' , mainController.index);
 
-router.get('/products' , mainController.products);
-
+// Rutas login y logout con validaciones
 router.get('/login' , mainController.loginForm);
 router.post('/login' , validationsLoginForm, mainController.loginAuth);
 router.get('/logout' , mainController.logout);
 
+// Rutas registro de usuario con validaciones
 router.get('/register' , mainController.registerForm);
 router.post('/register' , validationsRegisterForm, mainController.register);
 
