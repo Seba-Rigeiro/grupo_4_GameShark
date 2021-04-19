5402d2530b7f1cf5004b3286a39b2256185108f1
Sebastian Rigeiro <seba_rigeiro@yahoo.com.ar>
2021-04-19 00:46:38 -0300

    correccion de ruta edit y agregado de ruta para listar por platform

--- src/routes/productsrouter.js
+++ src/routes/productsrouter.js
@@ -5,9 +5,11 @@ const path = require ('path')
 const productsController = require('../controllers/productscontroller');
 
 const storage = multer.diskStorage ({
+    // Indica donde se guarda la nueva imagen
     destination: (req, file, callback) => {
         callback (null, path.join(__dirname, '../../public/img/products'))
     },
+    // Genera el nombre de la nueva imagen
     filename: (req, file, callback) => {
         const newFlieName = 'product-' + Date.now() + path.extname (file.originalname)
         callback (null, newFlieName)
@@ -16,12 +18,17 @@ const storage = multer.diskStorage ({
 
 const upload = multer ({ storage: storage})
 
+// Rutas creacion de productos con multer para las imagenes
 router.get('/create' , productsController.createForm);
 router.post('/create' , upload.single ('product_image'), productsController.create);
 
+// Rutas para listar productos y detalle de productos
+router.get('/products' , productsController.products);
 router.get('/products/:id' , productsController.detail);
+router.get('/platform/:id' , productsController.productsByPlatform);
 
-router.get('/edit' , productsController.editForm);
+// Rutas para editar producto
+router.get('/edit/:id' , productsController.editForm);
 router.put('/edit' , productsController.edit);
 
 module.exports = router;
