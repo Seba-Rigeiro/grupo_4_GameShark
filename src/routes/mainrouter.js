b847e73deedde27226f1cfbbc22df0f417bae6ef
Sebastian Rigeiro <seba_rigeiro@yahoo.com.ar>
2021-04-20 15:57:56 -0300

    consiguracion de multer para subir imagen de usuario

--- src/routes/mainrouter.js
+++ src/routes/mainrouter.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const multer = require ('multer');
+const path = require ('path')
 const {body} = require ('express-validator')
 const mainController = require('../controllers/maincontroller');
+// Multer para la imagen de usuario
+const storage = multer.diskStorage ({
+    // Indica donde se guarda la nueva imagen
+    destination: (req, file, callback) => {
+        callback (null, path.join(__dirname, '../../public/img/users'))
+    },
+    // Genera el nombre de la nueva imagen
+    filename: (req, file, callback) => {
+        const newFlieName = 'user-' + Date.now() + path.extname (file.originalname)
+        callback (null, newFlieName)
+    }
+})
+
+const upload = multer ({ storage: storage})
 
 // Validaciones para formulario de registro
 const validationsRegisterForm = [
@@ -27,7 +42,7 @@ router.get('/logout' , mainController.logout);
 
 // Rutas registro de usuario con validaciones
 router.get('/register' , mainController.registerForm);
-router.post('/register' , validationsRegisterForm, mainController.register);
+router.post('/register' , upload.single ('user_image'), validationsRegisterForm, mainController.register);
 
 router.get('/mycart' , mainController.mycart);
 
