b35150ac3f34b18fcd345562d14e5642c43edc0d
Sebastian Rigeiro <seba_rigeiro@yahoo.com.ar>
2021-05-01 01:30:24 -0300

    Actualizacion de la base de datos

--- src/routes/apiRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const apiController = require('../controllers/apiController');
-
-
-router.get ('/products', apiController.products);
-router.get ('/products/:id', apiController.productDetail);
-
-module.exports = router;
